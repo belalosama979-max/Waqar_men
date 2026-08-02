7268955a0998504da10275cfb7b79cfca68ad8ec
belalosama979-max <belalosama979@gmail.com>
2026-08-03 00:33:21 +0300

    fix: remove turbopack config for vercel

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {},
 };
 
 module.exports = withPWA(nextConfig);
